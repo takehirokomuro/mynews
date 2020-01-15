29158f198a9998d9403e5294ce4fc82b27bfdd8d
EC2 Default User <ec2-user@ip-172-31-20-80.us-east-2.compute.internal>
2020-01-15 02:16:30 +0000

    text14

---
+++ database/migrations/2020_01_15_014830_create_profile_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title'); // 氏名、性別、趣味を保存するカラム
+            $table->string('body');  // 自己紹介欄を保存するカラム
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
